35c9990a10f56e1eb1a66a51ead07982cfcc6057
vijayC <vijaychendrayan@gmail.com>
2017-12-26 15:50:39 +0530

    Handle multiple files in CLI
    Modified the program to handle multiple file(s)  passed as input to the
    Engine.

--- cerner/automation/AutomationEngine.java
+++ cerner/automation/AutomationEngine.java
@@ -1,18 +1,16 @@
 package com.cerner.automation;
 
-
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-
 import java.io.IOException;
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.Date;
 import org.w3c.dom.Element;
-
 // For XML
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -27,13 +25,14 @@ import org.w3c.dom.NodeList;
 
 import java.io.*;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 /**
- * Created by VC024129 on 5/6/2017.
+ * Created by Vijay Chendrayan Chandrasekar on 5/6/2017.
  */
 public class AutomationEngine  {
 
-    public static void main(String []args) throws InterruptedException, NullPointerException,IOException,ArrayIndexOutOfBoundsException, ParserConfigurationException{
+    public static void execute(String filePath)throws InterruptedException, NullPointerException,IOException,ArrayIndexOutOfBoundsException, ParserConfigurationException{
         Engine engine = new Engine();
         int prcsReturnValue =0;
         int totalTestCase = 0;
@@ -42,10 +41,18 @@ public class AutomationEngine  {
         String currentTestCaseID = " ";
         String currentTestCaseDescr = " ";
         String testCaseStatus = "PASS";
+        String cmdFileName;
+        String[] tempStr = filePath.split("\\\\");
+        String tempStr2 = tempStr[tempStr.length-1];
+        System.out.println("File Name for temp2 is : "+ tempStr2);
+        cmdFileName = tempStr2.substring(0,tempStr2.length()-5);
+        System.out.println("The File name is : "+cmdFileName);
+
         DateFormat autoDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         DateFormat htmlFileDateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
         Date fileDate = new Date();
-        String autoFileName = htmlFileDateFormat.format(fileDate).toString();
+        String autoFileName = htmlFileDateFormat.format(fileDate);
+        autoFileName = cmdFileName+"_"+autoFileName;
         System.out.println("File Name : "+autoFileName);
         // For XML//
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -55,20 +62,12 @@ public class AutomationEngine  {
         document.appendChild(rootElement);
         Element originalRoot = rootElement;
         // - END- XML
-        //String excelFilePath = "C:\\Users\\VC024129\\Documents\\Vijay\\TestFrameWork\\TestParameterPPM4.xlsx";
-        //String excelFilePath = "C:\\Users\\VC024129\\Documents\\Vijay\\TestFrameWork\\TestParameterHomeTest.xlsx";
-        //String excelFilePath = "C:\\Users\\VC024129\\Documents\\Vijay\\TestFrameWork\\CernerDotCom.xlsx";
-        //String excelFilePath = "C:\\Users\\VC024129\\Documents\\Vijay\\TestFrameWork\\CernerDotComDemo.xlsx";
-        String excelFilePath = args[0];
+        //String excelFilePath = args[0];
+        String excelFilePath = filePath;
 
         String driverType,driverPath,driverProp,screenShotFilePath,xmlFilePath,xslFilePath,htmlFilePath,htmlFileName;
+        String xmlFileName;
 
-        System.out.println("Argument Length : "+args.length);
-        if(args.length > 0){
-            for(int kk=0;kk<args.length;kk++){
-                System.out.println("Argument "+kk+" : "+args[kk]);
-            }
-        }
         // Setting up Web Driver
         try{
             ExcelFileDriver excelFileDriver = new ExcelFileDriver();
@@ -77,15 +76,6 @@ public class AutomationEngine  {
             Sheet excelSheetTestCase = null;
             Iterator<Row> rowIterator = null;
             Row row = null;
-            //Get Browser Configuration from "Configuration" worksheet
-            //Sheet excelSheetTestCase = excelWorkbook.getSheetAt(1);
-            //Iterator<Row> rowIterator = excelSheetTestCase.iterator();
-            //Row row = rowIterator.next();
-            //driverType = row.getCell(0).getRichStringCellValue().getString();
-            //driverPath = row.getCell(1).getRichStringCellValue().getString();
-            //driverProp = row.getCell(2).getRichStringCellValue().getString();
-            //System.out.println(driverType+" "+driverPath+" "+driverProp);
-            //engine.setWebDriver(driverType,driverProp,driverPath);
             //Get XML File path
             //----- Start-----
             Properties prop = new Properties();
@@ -96,26 +86,12 @@ public class AutomationEngine  {
             htmlFilePath = prop.getProperty("HTMLFILE");
             screenShotFilePath = prop.getProperty("SCREENSHOTFILE");
             input.close();
-
-
-
             //----- End ------
-            /*excelSheetTestCase = excelWorkbook.getSheetAt(3);
-            rowIterator = excelSheetTestCase.iterator();
-            row = rowIterator.next();
-            xmlFilePath = row.getCell(0).getRichStringCellValue().getString();
-            row = rowIterator.next();
-            xslFilePath = row.getCell(0).getRichStringCellValue().getString();
-            row = rowIterator.next();
-            //Result HTML File path / name
-            htmlFilePath = row.getCell(0).getRichStringCellValue().getString(); */
-            htmlFileName = "Automation_"+autoFileName+".html";
+            htmlFileName = "Auto_"+autoFileName+".html";
+            xmlFileName = autoFileName+".xml";
             htmlFilePath = htmlFilePath+"/"+htmlFileName;
+            xmlFilePath = xmlFilePath+"/"+xmlFileName;
             //Get Screenshot file path
-            /* excelSheetTestCase = excelWorkbook.getSheetAt(2);
-            rowIterator = excelSheetTestCase.iterator();
-            row = rowIterator.next();
-            screenShotFilePath = row.getCell(0).getRichStringCellValue().getString();*/
             engine.setScreenShotPath(screenShotFilePath);
             Sheet sheet = excelWorkbook.getSheetAt(0);
             rowIterator = sheet.iterator();
@@ -201,9 +177,6 @@ public class AutomationEngine  {
             DOMSource source = new DOMSource(document);
             StreamResult result = new StreamResult(new File(xmlFilePath));
             transformer.transform(source,result);
-            //StreamResult consoleResult = new StreamResult(System.out);
-            //transformer.transform(source,consoleResult);
-            //Thread.sleep(5000);
             //Generate HTML report using XSLT
             System.out.println("--------");
             System.out.println("XML File : "+xmlFilePath);
@@ -212,18 +185,30 @@ public class AutomationEngine  {
             TransformerFactory transformerFactoryHTML = TransformerFactory.newInstance();
             Transformer transformHTML = transformerFactoryHTML.newTransformer(new javax.xml.transform.stream.StreamSource(xslFilePath));
             transformHTML.transform(new javax.xml.transform.stream.StreamSource(xmlFilePath), new javax.xml.transform.stream.StreamResult(htmlFilePath));
-            //engine.webDriver.get(htmlFilePath);
-            //Thread.sleep(5000);
-            //engine.webDriver.close();
             excelWorkbook.close();
             System.out.println("---- Summary ---");
-            //System.out.println("Total Test Cases : "+ totalTestCase);
-            //System.out.println("Test Case Passed : "+ passTestCaseCount);
-            //System.out.println("Test Case Failed : "+ failTestCaseCount);
+
         }catch (NullPointerException e){e.printStackTrace();}
         catch (Exception e){e.printStackTrace();
 
         }
+
+    }
+
+    public static void main(String []args) throws InterruptedException, NullPointerException,IOException,ArrayIndexOutOfBoundsException, ParserConfigurationException{
+
+        String cmdFilePath = " ";
+        String cmdFileName = " ";
+        System.out.println("Argument Length : "+args.length);
+        if(args.length > 0){
+            for(int kk=0;kk<args.length;kk++){
+                cmdFilePath = args[kk];
+                System.out.println("Argument "+kk+" : "+cmdFilePath);
+                execute(cmdFilePath);
+            }
+
+        }
+
 
         /*System.exit(returnStatusOS);*/
     }
